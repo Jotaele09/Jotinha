f6fe1337c4904a2b53c45be7cdcfa92fdf2455b6
Jotaele09 <araujo.joao15@escola.pr.gov.br>
2025-09-24 11:07:20 -0300

    a.

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-function criarCartão(categoria, pergunta,   resposta){
-console.log(categoria, pergunta, resposta);
+function criarCartão(categoria, pergunta,   resposta) {
+let container = document.getElementById('container')
+let cartao = document.createElement('article')
+cartao.className = 'cartao'
+cartao.innerHTML = 
+ <div class="cartao_conteudo">
+                    <h3> Game Hades  <h3>
+                            <div class="cartao_conteudo_pergunta">
+                                <p> Quem é o pai de Zagreus?  </p>
+                            </div>
+                            <div class="cartao_conteudo_pergunta">
+                                <p> Hades<p>
+                            </div>
+                </div>
 
 
 }
