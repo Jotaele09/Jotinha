97cdc17da02662839ffb1a1f73e8c31c869203bb
Jotaele09 <araujo.joao15@escola.pr.gov.br>
2025-10-01 21:31:36 -0300

    a

--- perguntas.js
+++ perguntas.js
@@ -5,26 +5,26 @@ criaCartao(
 )
 criaCartao(
     'Dead By Daylight - Pergunta 1:',
-    'O que é tunelar?',
-    'É quando o killer foca em uma só pessoa em vez de perseguir os outros players.'
+    'Qual é o primeiro boss do jogo?',
+    'O porteiro'
 )
 criaCartao(
     'Pergunta 2:',
-    'O que são perks?',
-    'São habilidades, de sobrevivente e killer, que podem ajudar em certos atributos na partida. exemplo: Deja Vu - de sobrevivente - Mostra os 3 geradores que estão mais proximos um do outro, e da 6% de bonus de velocidade ao fazer os geradores marcados. exemplo: Lightborn - de killer - As auras dos sobreviventes que tentam te cegar são mostradas por 10 segundos. Da imunidade contra ser cegado.',
+    'O que é o protagonista de Dead cells ?',
+    'Um Homúnculo.',
 )
 criaCartao(
     'Pergunta 3:',
-    'O que é "mori"?',
-    'É uma oferenda que você poe no killer, para conseguir a opção de matar sem ser no gancho (ganha a opção após o sobrevivente chegar no gancho 2). Faz uma animação especial que é diferente para cada killer.',
+    'Qual a dificuldade máxima do jogo?',
+    '5 células',
 )
 criaCartao(
     'Pergunta 4:',
-    'O que é "loopar o killer"?',
-    'É segurar o killer em perseguição, para que os outros sobreviventes façam geradores, se curem, salvem outros sobreviventes do gancho etc...',
+    'Qual o boss final verdadeiro?',
+    'É seg',
 )
 criaCartao(
     'Pergunta 5:',
-    'O que é slugar?',
-    'É quando o killer deixa o sobrevivente no estado morrendo, e não coloca no gancho, deixa o survivor no chão e vai atras de outra perseguição',
+    'Quem era o rei?',
+    'O próprio protagonista',
 )
